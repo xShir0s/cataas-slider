7714da84d2648c42a4674fa3ff6652a9dd667cd0
xShir0s <mtodys2006@gmail.com>
2024-11-08 13:58:03 +0100

    dodanie plikow

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CatassSlider
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace CatassSlider
+{
+    public partial class MainPage : ContentPage
+    {
+    
+
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            double value = args.NewValue;
+            slider.MaximumHeightRequest = value;
+          
+           
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            string link = "https://cataas.com/cat";
+            slider.Source = link;
+       
+        }
+        private void OnCounterClicked2(object sender, EventArgs e)
+        {
+            string link = "https://cataas.com/cat/gif";
+            slider.Source = link;
+        
+        }
+    }
+
+}
